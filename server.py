82e8b1aca8b95cc5f3ca53e54ffeac24be855e9a
Aimen <aimenhamed@live.com.au>
2021-08-17 18:37:39 +1000

    added post route for adding courses

--- server.py
+++ server.py
@@ -1,22 +1,41 @@
-from flask import Flask
+from flask import Flask, Response, request
 from json import dumps
+
 app = Flask(__name__)
 
-course_list = [
+courses = [
     {"id": 1, "name": "COMP1511"},
     {"id": 2, "name": "COMP1521"},
     {"id": 3, "name": "COMP1531"},
 ]
 
 
-@app.route("/", methods=['get'])
+@app.route("/", methods=['GET'])
 def home():
     return "Hello World!"
 
 
-@app.route("/api/courses", methods=['get'])
-def courses():
-    return dumps(course_list)
+@app.route("/api/courses", methods=['GET'])
+def get_courses():
+    return dumps(courses)
+
+
+@app.route("/api/courses", methods=["POST"])
+def add_course():
+    req = request.get_json()
+    name = req["name"]
+    if len(name) != 8:
+        return Response(
+            "Name is required and should be 8 chars long, e.g. COMP1511",
+            status=400,
+        )
+    course = {
+        "id": len(courses) + 1,
+        "name": name,
+    }
+
+    courses.append(course)
+    return course
 
 
 if __name__ == "__main__":
