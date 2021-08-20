1a57b4da868a0f602dd3d01de1547e9ecf271034
Aimen <aimenhamed@live.com.au>
2021-08-20 12:15:27 +1000

    CORS fix

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 from flask import Flask, Response, request
 from json import dumps
 from courses import courses
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/", methods=['GET'])
